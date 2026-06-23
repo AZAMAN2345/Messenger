2b3b94df72795774b546bcf805531f8f4ce127f9
AZAMAN2345 <edosaehis4@gmail.com>
2026-06-23 14:14:53 +0100

    commit

--- simple-chat/lib/src/App.jsx
+++ simple-chat/lib/src/App.jsx
@@ -1,20 +1,38 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import Conversation from "./pages/Conversation";
 import Register from "./pages/Register";
+import { getStoredUser } from "./api";
+
+function ProtectedRoute({ children }) {
+  return getStoredUser() ? children : <Navigate replace to="/login" />;
+}
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate replace to="/login" />} />
       <Route path="/login" element={<Login />} />
-
-      <Route path="/chat" element={<Chat />} />
-
-      <Route path="/conversation/:conversationId" element={<Conversation />} />
-
-      <Route path="/register" element={<Register/>} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/chat"
+        element={
+          <ProtectedRoute>
+            <Chat />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/conversation/:conversationId"
+        element={
+          <ProtectedRoute>
+            <Conversation />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<Navigate replace to="/login" />} />
     </Routes>
   );
 }
